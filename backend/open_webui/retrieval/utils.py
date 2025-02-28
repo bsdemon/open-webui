b00b43965a110db22d6e23a2f6399f36a93e431c
Iliya Iliev <bsdemon@gmail.com>
2025-03-01 00:22:13 +0200

    Increase openai resuqst timeout

--- backend/open_webui/retrieval/utils.py
+++ backend/open_webui/retrieval/utils.py
@@ -566,6 +566,7 @@ def generate_openai_batch_embeddings(
                 ),
             },
             json={"input": texts, "model": model},
+            timeout=300.00,
         )
         r.raise_for_status()
         data = r.json()
